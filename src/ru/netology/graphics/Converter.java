41551e775ab4bc156bd263f20a7056199dbccf4b
Soul <top.secret.work@yandex.ru>
2021-10-25 18:32:13 +0300

    Update to working version

--- src/ru/netology/graphics/Converter.java
+++ src/ru/netology/graphics/Converter.java
@@ -14,14 +14,13 @@ import java.net.URL;
 
 public class Converter implements TextGraphicsConverter {
 
-    Schema schema = new Schema();
+    TextColorSchema schema = new Schema();
     private int maxWidth = 0;
     private int maxHeight = 0;
     private double maxRatio = 0;
 
     @Override
     public String convert(String url) throws IOException, BadImageSizeException {
-        // Вот так просто мы скачаем картинку из интернета :)
         BufferedImage img = ImageIO.read(new URL(url));
 
         int width = img.getWidth();
@@ -30,96 +29,37 @@ public class Converter implements TextGraphicsConverter {
         double ratioWidth = (double) width / height;
         double ratioHeight = (double) height / width;
         double ratio = Math.max(ratioWidth, ratioHeight);
-        System.out.println(ratio + " - " + maxRatio);
 
-        if (maxRatio > 0) {
-            System.out.println("yes");
+        if (ratio > maxRatio && maxRatio > 0) {
             throw new BadImageSizeException(ratio, maxRatio);
         }
 
-        // Получить правильный размер сжатия, пропорционально.
-        /*
-            W / w = H / h
-            h = H * w / W
-            w = W * h / H
-         */
-
-        /*
-        * Ужмем картинку по найденому коэфициенту
-        */
+        // Ужмем картинку по найденому коэфициенту
         if ((maxWidth < width && maxWidth != 0) || (maxHeight < height && maxHeight != 0)) {
             double k = Math.max((double) width / maxWidth, (double) height / maxHeight);
-            System.out.println("w: " + width + ", h: " + height);
-            System.out.println("k1: " + (double) width / maxWidth + ", k2: " + (double) height / maxHeight);
             width = (int) (width / k);
             height = (int) (height / k);
         }
-        System.out.println("w: " + width + " h: " + height);
 
-
-        // Если конвертер попросили проверять на максимально допустимое
-        // соотношение сторон изображения, то вам здесь надо сделать эту проверку,
-        // и, если картинка не подходит, выбросить исключение BadImageSizeException.
-        // Чтобы получить ширину картинки, вызовите img.getWidth(), высоту - img.getHeight()
-
-        // Если конвертеру выставили максимально допустимые ширину и/или высоту,
-        // вам надо по ним и по текущим высоте и ширине вычислить новые высоту
-        // и ширину.
-        // Соблюдение пропорций означает, что вы должны уменьшать ширину и высоту должны
-        // в одинаковое количество раз.
-        // Пример 1: макс. допустимые 100x100, а картинка 500x200. Новый размер
-        // будет 100x40 (в 5 раз меньше).
-        // Пример 2: макс. допустимые 100x30, а картинка 150x15. Новый размер
-        // будет 100x10 (в 1.5 раза меньше).
-        // Подумайте, какими действиями можно вычислить новые размеры.
-        // Не получается? Спросите вашего руководителя по курсовой, поможем!
-        /*int newWidth = ???;
-        int newHeight = ???;*/
-
-        // Теперь нам надо попросить картинку изменить свои размеры на новые.
-        // Последний параметр означает, что мы просим картинку плавно сузиться
-        // на новые размеры. В результате мы получаем ссылку на новую картинку, которая
-        // представляет собой суженную старую.
         Image scaledImage = img.getScaledInstance(width, height, BufferedImage.SCALE_SMOOTH);
-
-        // Теперь сделаем её чёрно-белой. Для этого поступим так:
-        // Создадим новую пустую картинку нужных размеров, заранее указав последним
-        // параметром чёрно-белую цветовую палитру:
         BufferedImage bwImg = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
-        // Попросим у этой картинки инструмент для рисования на ней:
         Graphics2D graphics = bwImg.createGraphics();
-        // А этому инструменту скажем, чтобы он скопировал содержимое из нашей суженной картинки:
         graphics.drawImage(scaledImage, 0, 0, null);
 
-        // Теперь в bwImg у нас лежит чёрно-белая картинка нужных нам размеров.
-        // Вы можете отслеживать каждый из этапов, просто в любом удобном для
-        // вас моменте сохранив промежуточную картинку в файл через:
         ImageIO.write(bwImg, "png", new File("out.png"));
-        // После вызова этой инструкции у вас в проекте появится файл картинки out.png
 
-        // Теперь давайте пройдёмся по пикселям нашего изображения.
-        // Если для рисования мы просили у картинки .createGraphics(),
-        // то для прохода по пикселям нам нужен будет этот инструмент:
         WritableRaster bwRaster = bwImg.getRaster();
         int[] casheArray = new int[3];
         StringBuilder sb = new StringBuilder();
-
 
         for (int i = 0; i <= (bwRaster.getHeight() - 1); i++) {
             for (int j = 0; j < (bwRaster.getWidth() - 1); j++) {
                 int color = bwRaster.getPixel(j, i, casheArray)[0];
                 char c = schema.convert(color);
                 sb.append(c).append(c);
-            //??? //запоминаем символ c, например, в двумерном массиве или как-то ещё на ваше усмотрение
             }
             sb.append("<br>");
         }
-
-        // Осталось собрать все символы в один большой текст
-        // Для того, чтобы изображение не было слишком узким, рекомендую
-        // каждый пиксель превращать в два повторяющихся символа, полученных
-        // от схемы.
-
         return sb.toString(); // Возвращаем собранный текст.
     }
 
@@ -140,6 +80,6 @@ public class Converter implements TextGraphicsConverter {
 
     @Override
     public void setTextColorSchema(TextColorSchema schema) {
-
+        this.schema = schema;
     }
 }
--- src/ru/netology/graphics/Schema.java
+++ src/ru/netology/graphics/Schema.java
@@ -3,7 +3,17 @@ package ru.netology.graphics;
 import ru.netology.graphics.image.TextColorSchema;
 
 public class Schema implements TextColorSchema {
-    private static final char[] symbols = {'#', '$', '@', '%', '*', '+', '-', '\''};
+
+    //private static final char[] symbols = {'#', '$', '@', '%', '*', '+', '-', '\''};
+    private static char[] symbols = new char[8];
+
+    public Schema() {
+        symbols = new char[]{'#', '$', '@', '%', '*', '+', '-', '\''};
+    }
+
+    public Schema(Character a, Character b, Character c, Character d, Character e, Character f, Character g, Character h) {
+        symbols = new char[]{a, b, c, d, e, f, g, h};
+    }
 
     @Override
     public char convert(int color) {
